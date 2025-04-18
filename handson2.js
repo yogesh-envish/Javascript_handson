f91b1212f72b582277bcd91ff00ef8d193660ef1
yogesh-envish <yogeshchinnu001@gmzil.com>
2025-04-18 20:37:08 +0530

    Added few codes

--- handson2.js
+++ handson2.js
@@ -1,1 +1,14 @@
-//New Program
+// Check Blank String
+
+is_Blank=function(str){
+
+    if(str==""||str==null||str==" "){
+        return true;
+    }else{
+       return false;
+    }
+
+}
+
+console.log(is_Blank(' '));
+//console.log(is_Blank('Yogesh'))
